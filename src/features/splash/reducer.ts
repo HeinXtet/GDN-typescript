2838acbf424b0c171e7d005f3a7acb7a9022a219
heinhtet <deevvdd@gmail.com>
2020-02-06 23:40:24 +0630

    add masterData Reducer

--- src/features/splash/reducer.ts
+++ src/features/splash/reducer.ts
@@ -1,21 +1,26 @@
-import { createReducer } from 'typesafe-actions';
-import { checkAvailableIp } from './actions';
-import { combineReducers } from 'redux';
-import { IpModel } from 'IpModel';
+import {createReducer} from 'typesafe-actions';
+import {checkAvailableIp, getMasterData} from './actions';
+import {combineReducers} from 'redux';
+import {IpModel} from 'IpModel';
+import {MasterData} from 'MasterData';
 
+export const errorInCheckAvailableIp = createReducer(
+  null as String,
+).handleAction([checkAvailableIp.failure], (state, action) => action.payload);
 
+export const successInCheckAvailableIp = createReducer(
+  null as IpModel,
+).handleAction([checkAvailableIp.success], (state, action) => action.payload);
 
-export const errorInCheckAvailableIp = createReducer(null as String)
-    .handleAction([checkAvailableIp.failure], (state, action) => action.payload)
-
-
-export const successInCheckAvailableIp = createReducer(null as IpModel)
-    .handleAction([checkAvailableIp.success], (state, action) => action.payload)
-
+export const successMasterData = createReducer(null as MasterData).handleAction(
+  [getMasterData.success],
+  (state, action) => action.payload,
+);
 
 const checkIpReducer = combineReducers({
-    successInCheckAvailableIp,
-    errorInCheckAvailableIp
+  successInCheckAvailableIp,
+  errorInCheckAvailableIp,
+  successMasterData,
 });
 
 export default checkIpReducer;
